dfb23189a8ddeee23d13d0de568813025233db6f
Ian Deans <ideans715@gmail.com>
2018-07-05 23:59:01 -0700

    Add update method to Transaction class.

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -42,7 +42,28 @@ describe('Transaction', () => {
 
     it('does not create the transaction', () => {
       expect(transaction).toEqual(undefined)
+    });
+  });
+
+  describe('updating a transaction', () => {
+    
+    let nextAmount, nextRecipient;
+    
+    beforeEach(() => {
+      nextAmount = 20;
+      nextRecipient = 'n3xt-4ddr355';
+      transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    });
+
+    it (`subtracts the next amount from the sender's output`, () => {
+      expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+        .toEqual(wallet.balance - amount - nextAmount);
+    });
+
+    it('outputs an amount for the next recipient', () => {
+      expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+        .toEqual(nextAmount);
     })
-  })
+  });
 
 })
