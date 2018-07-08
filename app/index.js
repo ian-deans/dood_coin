d55e9b267ec0bbd51c1e600e87432c765e7c8af6
Ian Deans <ideans715@gmail.com>
2018-07-08 14:20:11 -0700

    Add broadcast transaction feature to p2p server class

--- app/index.js
+++ app/index.js
@@ -1,36 +1,50 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const logger = require('morgan')
-const Blockchain = require('../blockchain')
-const P2pServer = require('./p2p-server')
+const express = require('express');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
-const HTTP_PORT = process.env.HTTP_PORT || 3001
 
-const app = express()
-const bc = new Blockchain()
-const p2pServer = new P2pServer(bc)
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
-app.use(bodyParser.json())
-app.use(logger('dev'))
+const app = express();
+const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
+const p2pServer = new P2pServer(bc, tp);
 
-app.get('/blocks', (request, response) => 
-  response.json(bc.chain))
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.get('/blocks', (request, response) =>
+  response.json(bc.chain));
 
 app.post('/mine', (request, response) => {
-  const {data} = request.body
+  const {data} = request.body;
   if (!data) {
-    throw new Error('Body must have property "data".')
+    throw new Error('Body must have property "data".');
   }
 
-  const block = bc.addBlock(data)
-  console.log(`New block added: ${block.toString()}`)
+  const block = bc.addBlock(data);
+  console.log(`New block added: ${block.toString()}`);
 
-  p2pServer.syncChains()
-  response.redirect('/blocks')
+  p2pServer.syncChains();
+  response.redirect('/blocks');
 })
 
+app.get('/transactions', (request, response) => response.json(tp.transactions));
+
+app.post('/transact', (request, response) => {
+  const {recipient, amount} = request.body;
+  const transaction = wallet.createTransaction(recipient, amount, tp);
+  p2pServer.broadcastTransaction(transaction);
+
+  response.redirect('/transactions');
+});
 
 
-app.listen(HTTP_PORT, () => 
+app.listen(HTTP_PORT, () =>
   console.log(`Listening on port ${HTTP_PORT}`))
 p2pServer.listen()
