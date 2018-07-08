402ab397c7bbeb80e6b114c158374f479da1a25c
Ian Deans <ideans715@gmail.com>
2018-07-08 11:16:02 -0700

    Add tests for Wallet class for creating new transactions

--- wallet/index.js
+++ wallet/index.js
@@ -1,23 +1,43 @@
 const {INITIAL_BALANCE} = require('../config')
+const Transaction = require('./Transaction')
 const ChainUtil = require('../chain-util')
 
 class Wallet {
   constructor() {
-    this.balance = INITIAL_BALANCE
-    this.keyPair = ChainUtil.genKeyPair()
-    this.publicKey = this.keyPair.getPublic().encode('hex')
+    this.balance = INITIAL_BALANCE;
+    this.keyPair = ChainUtil.genKeyPair();
+    this.publicKey = this.keyPair.getPublic().encode('hex');
   }
 
   toString() {
     return `Wallet -
         publicKey:    ${this.publicKey.toString()}
         balance:      ${this.balance}
-      `
+      `;
   }
 
   sign(dataHash) {
-    return this.keyPair.sign(dataHash)
+    return this.keyPair.sign(dataHash);
   }
-}
 
-module.exports = Wallet
+  createTransaction(recipient, amount, transactionPool) {
+    if (amount > this.balance) {
+      console.log(`Amount: ${amount} exceeds the current balance: ${this.balance}`);
+      return;
+    }
+
+    let transaction = transactionPool.existingTransaction(this.publicKey);
+
+    if (transaction) {
+      transaction.update(this, recipient, amount);
+    } else {
+      transaction = Transaction.newTransaction(this, recipient, amount);
+      transactionPool.updateOrAddTransaction(transaction);
+    }
+
+    return transaction;
+  }
+
+};
+
+module.exports = Wallet;
